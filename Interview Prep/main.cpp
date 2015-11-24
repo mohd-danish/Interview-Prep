864e7cc18542e1f0700f1f7d72d2f06818fe167a
mohd-danish <mohd.ansari01@snapdeal.com>
2015-11-24 15:24:16 +0530

    Initial Commit

---
+++ Interview Prep/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Interview Prep
+//
+//  Created by Tux on 24/11/15.
+//  Copyright © 2015 Tux_Ansari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
